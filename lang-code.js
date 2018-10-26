f850ff8693a358bbc9b4780caecafb81fe3463b5
jian.lin <bruce.lin@royole.com>
2018-10-26 11:42:19 +0800

    新增语言代码

---
+++ lang-code.js
@@ -0,0 +1,382 @@
+export default [
+  {
+    name: "Afrikaans - South Africa",
+    code: "af-ZA",
+    chineseName: "南非荷兰语 - 南非"
+  },
+  {
+    name: "Albanian - Albania",
+    code: "sq-AL",
+    chineseName: "阿尔巴尼亚人 - 阿尔巴尼亚"
+  },
+  {
+    name: "Arabic - Algeria",
+    code: "ar-DZ",
+    chineseName: "阿拉伯语 - 阿尔及利亚"
+  },
+  { name: "Arabic - Bahrain", code: "ar-BH", chineseName: "阿拉伯语 - 巴林" },
+  { name: "Arabic - Egypt", code: "ar-EG", chineseName: "阿拉伯语 - 埃及" },
+  { name: "Arabic - Iraq", code: "ar-IQ", chineseName: "阿拉伯语 - 伊拉克" },
+  { name: "Arabic - Jordan", code: "ar-JO", chineseName: "阿拉伯语 - 约旦" },
+  { name: "Arabic - Kuwait", code: "ar-KW", chineseName: "阿拉伯语 - 科威特" },
+  { name: "Arabic - Lebanon", code: "ar-LB", chineseName: "阿拉伯语 - 黎巴嫩" },
+  { name: "Arabic - Libya", code: "ar-LY", chineseName: "阿拉伯语 - 利比亚" },
+  { name: "Arabic - Morocco", code: "ar-MA", chineseName: "阿拉伯语 - 摩洛哥" },
+  { name: "Arabic - Oman", code: "ar-OM", chineseName: "阿拉伯语 - 阿曼" },
+  { name: "Arabic - Qatar", code: "ar-QA", chineseName: "阿拉伯语 - 卡塔尔" },
+  {
+    name: "Arabic - Saudi Arabia",
+    code: "ar-SA",
+    chineseName: "阿拉伯语 - 沙特阿拉伯"
+  },
+  { name: "Arabic - Syria", code: "ar-SY", chineseName: "阿拉伯语 - 叙利亚" },
+  { name: "Arabic - Tunisia", code: "ar-TN", chineseName: "阿拉伯语 - 突尼斯" },
+  {
+    name: "Arabic - United Arab Emirates",
+    code: "ar-AE",
+    chineseName: "阿拉伯语 - 阿拉伯联合酋长国"
+  },
+  { name: "Arabic - Yemen", code: "ar-YE", chineseName: "阿拉伯语 - 也门" },
+  {
+    name: "Armenian - Armenia",
+    code: "hy-AM",
+    chineseName: "亚美尼亚 - 亚美尼亚"
+  },
+  {
+    name: "Azeri (Cyrillic) - Azerbaijan",
+    code: "Cy-az-AZ",
+    chineseName: "阿塞拜疆（西里尔） - 阿塞拜疆"
+  },
+  {
+    name: "Azeri (Latin) - Azerbaijan",
+    code: "Lt-az-AZ",
+    chineseName: "阿塞拜疆（拉丁） - 阿塞拜疆"
+  },
+  {
+    name: "Basque - Basque",
+    code: "eu-ES",
+    chineseName: "巴斯克语 - 巴斯克语"
+  },
+  {
+    name: "Belarusian - Belarus",
+    code: "be-BY",
+    chineseName: "白俄罗斯 - 白俄罗斯"
+  },
+  {
+    name: "Bulgarian - Bulgaria",
+    code: "bg-BG",
+    chineseName: "保加利亚 - 保加利亚"
+  },
+  {
+    name: "Catalan - Catalan",
+    code: "ca-ES",
+    chineseName: "加泰罗尼亚语 - 加泰罗尼语"
+  },
+  { name: "Chinese - China", code: "zh-CN", chineseName: "中文 - 中国" },
+  {
+    name: "Chinese - Hong Kong SAR",
+    code: "zh-HK",
+    chineseName: "中文 - 香港特别行政区"
+  },
+  {
+    name: "Chinese - Macau SAR",
+    code: "zh-MO",
+    chineseName: "中文 - 澳门特别行政区"
+  },
+  { name: "Chinese - Singapore", code: "zh-SG", chineseName: "中文 - 新加坡" },
+  { name: "Chinese - Taiwan", code: "zh-TW", chineseName: "中文 - 台湾" },
+  { name: "Chinese (Simplified)", code: "zh-CHS", chineseName: "简体中文）" },
+  { name: "Chinese (Traditional)", code: "zh-CHT", chineseName: "繁体中文）" },
+  {
+    name: "Croatian - Croatia",
+    code: "hr-HR",
+    chineseName: "克罗地亚 - 克罗地亚"
+  },
+  {
+    name: "Czech - Czech Republic",
+    code: "cs-CZ",
+    chineseName: "捷克 - 捷克共和国"
+  },
+  { name: "Danish - Denmark", code: "da-DK", chineseName: "丹麦语 - 丹麦语" },
+  {
+    name: "Dhivehi - Maldives",
+    code: "div-MV",
+    chineseName: "Dhivehi - 马尔代夫"
+  },
+  { name: "Dutch - Belgium", code: "nl-BE", chineseName: "荷兰 - 比利时" },
+  {
+    name: "Dutch - The Netherlands",
+    code: "nl-NL",
+    chineseName: "荷兰 - 荷兰"
+  },
+  {
+    name: "English - Australia",
+    code: "en-AU",
+    chineseName: "英文 - 澳大利亚"
+  },
+  { name: "English - Belize", code: "en-BZ", chineseName: "英语 - 伯利兹" },
+  { name: "English - Canada", code: "en-CA", chineseName: "英文 - 加拿大" },
+  { name: "English - Caribbean", code: "en-CB", chineseName: "英语 - 加勒比" },
+  { name: "English - Ireland", code: "en-IE", chineseName: "英语 - 爱尔兰" },
+  { name: "English - Jamaica", code: "en-JM", chineseName: "英语 - 牙买加" },
+  {
+    name: "English - New Zealand",
+    code: "en-NZ",
+    chineseName: "英语 - 新西兰"
+  },
+  {
+    name: "English - Philippines",
+    code: "en-PH",
+    chineseName: "英语 - 菲律宾"
+  },
+  { name: "English - South Africa", code: "en-ZA", chineseName: "英文 - 南非" },
+  {
+    name: "English - Trinidad and Tobago",
+    code: "en-TT",
+    chineseName: "英文 - 特立尼达和多巴哥"
+  },
+  {
+    name: "English - United Kingdom",
+    code: "en-GB",
+    chineseName: "英文 - 英国"
+  },
+  { name: "English - United States", code: "en-US", chineseName: "美国英语" },
+  { name: "English - Zimbabwe", code: "en-ZW", chineseName: "英语 - 津巴布韦" },
+  {
+    name: "Estonian - Estonia",
+    code: "et-EE",
+    chineseName: "爱沙尼亚语 - 爱沙尼亚"
+  },
+  {
+    name: "Faroese - Faroe Islands",
+    code: "fo-FO",
+    chineseName: "法罗群岛 - 法罗群岛"
+  },
+  { name: "Farsi - Iran", code: "fa-IR", chineseName: "波斯 - 伊朗" },
+  { name: "Finnish - Finland", code: "fi-FI", chineseName: "芬兰 - 芬兰" },
+  { name: "French - Belgium", code: "fr-BE", chineseName: "法语 - 比利时" },
+  { name: "French - Canada", code: "fr-CA", chineseName: "法语 - 加拿大" },
+  { name: "French - France", code: "fr-FR", chineseName: "法国 - 法国" },
+  { name: "French - Luxembourg", code: "fr-LU", chineseName: "法语 - 卢森堡" },
+  { name: "French - Monaco", code: "fr-MC", chineseName: "法语 - 摩纳哥" },
+  { name: "French - Switzerland", code: "fr-CH", chineseName: "法语 - 瑞士" },
+  {
+    name: "Galician - Galician",
+    code: "gl-ES",
+    chineseName: "加利西亚人 - 加利西亚人"
+  },
+  {
+    name: "Georgian - Georgia",
+    code: "ka-GE",
+    chineseName: "格鲁吉亚 - 格鲁吉亚"
+  },
+  { name: "German - Austria", code: "de-AT", chineseName: "德国 - 奥地利" },
+  { name: "German - Germany", code: "de-DE", chineseName: "德国 - 德国" },
+  {
+    name: "German - Liechtenstein",
+    code: "de-LI",
+    chineseName: "德语 - 列支敦士登"
+  },
+  { name: "German - Luxembourg", code: "de-LU", chineseName: "德国 - 卢森堡" },
+  { name: "German - Switzerland", code: "de-CH", chineseName: "德国 - 瑞士" },
+  { name: "Greek - Greece", code: "el-GR", chineseName: "希腊 - 希腊" },
+  { name: "Gujarati - India", code: "gu-IN", chineseName: "古吉拉特文 - 印度" },
+  { name: "Hebrew - Israel", code: "he-IL", chineseName: "希伯来语 - 以色列" },
+  { name: "Hindi - India", code: "hi-IN", chineseName: "印地文 - 印度" },
+  {
+    name: "Hungarian - Hungary",
+    code: "hu-HU",
+    chineseName: "匈牙利 - 匈牙利"
+  },
+  { name: "Icelandic - Iceland", code: "is-IS", chineseName: "冰岛 - 冰岛" },
+  { name: "Indonesian - Indonesia", code: "id-ID", chineseName: "印尼 - 印尼" },
+  { name: "Italian - Italy", code: "it-IT", chineseName: "意大利 - 意大利" },
+  {
+    name: "Italian - Switzerland",
+    code: "it-CH",
+    chineseName: "意大利语 - 瑞士"
+  },
+  { name: "Japanese - Japan", code: "ja-JP", chineseName: "日文 - 日本" },
+  { name: "Kannada - India", code: "kn-IN", chineseName: "卡纳达 - 印度" },
+  { name: "Kazakh - Kazakhstan", code: "kk-KZ", chineseName: "哈萨克斯坦" },
+  { name: "Konkani - India", code: "kok-IN", chineseName: "康卡尼 - 印度" },
+  { name: "Korean - Korea", code: "ko-KR", chineseName: "韩国 - 韩国" },
+  {
+    name: "Kyrgyz - Kazakhstan",
+    code: "ky-KZ",
+    chineseName: "吉尔吉斯斯坦 - 哈萨克斯坦"
+  },
+  {
+    name: "Latvian - Latvia",
+    code: "lv-LV",
+    chineseName: "拉脱维亚 - 拉脱维亚"
+  },
+  {
+    name: "Lithuanian - Lithuania",
+    code: "lt-LT",
+    chineseName: "立陶宛立陶宛"
+  },
+  { name: "Macedonian (FYROM)", code: "mk-MK", chineseName: "马其顿（FYROM）" },
+  { name: "Malay - Brunei", code: "ms-BN", chineseName: "马来文 - 文莱" },
+  { name: "Malay - Malaysia", code: "ms-MY", chineseName: "马来文 - 马来西亚" },
+  { name: "Marathi - India", code: "mr-IN", chineseName: "马拉地 - 印度" },
+  { name: "Mongolian - Mongolia", code: "mn-MN", chineseName: "蒙古 - 蒙古" },
+  {
+    name: "Norwegian (Bokmål) - Norway",
+    code: "nb-NO",
+    chineseName: "挪威语（Bokmål） - 挪威"
+  },
+  {
+    name: "Norwegian (Nynorsk) - Norway",
+    code: "nn-NO",
+    chineseName: "挪威（尼诺斯克） - 挪威"
+  },
+  { name: "Polish - Poland", code: "pl-PL", chineseName: "波兰语 - 波兰语" },
+  {
+    name: "Portuguese - Brazil",
+    code: "pt-BR",
+    chineseName: "葡萄牙语 - 巴西"
+  },
+  {
+    name: "Portuguese - Portugal",
+    code: "pt-PT",
+    chineseName: "葡萄牙 - 葡萄牙"
+  },
+  { name: "Punjabi - India", code: "pa-IN", chineseName: "旁遮普邦 - 印度" },
+  {
+    name: "Romanian - Romania",
+    code: "ro-RO",
+    chineseName: "罗马尼亚语 - 罗马尼亚"
+  },
+  { name: "Russian - Russia", code: "ru-RU", chineseName: "俄罗斯 - 俄罗斯" },
+  { name: "Sanskrit - India", code: "sa-IN", chineseName: "梵文 - 印度" },
+  {
+    name: "Serbian (Cyrillic) - Serbia",
+    code: "Cy-sr-SP",
+    chineseName: "塞尔维亚语（塞尔维亚语） - 塞尔维亚语"
+  },
+  {
+    name: "Serbian (Latin) - Serbia",
+    code: "Lt-sr-SP",
+    chineseName: "塞尔维亚（拉丁） - 塞尔维亚"
+  },
+  {
+    name: "Slovak - Slovakia",
+    code: "sk-SK",
+    chineseName: "斯洛伐克语 - 斯洛伐克"
+  },
+  {
+    name: "Slovenian - Slovenia",
+    code: "sl-SI",
+    chineseName: "斯洛文尼亚语 - 斯洛维尼亚"
+  },
+  {
+    name: "Spanish - Argentina",
+    code: "es-AR",
+    chineseName: "西班牙语 - 阿根廷"
+  },
+  {
+    name: "Spanish - Bolivia",
+    code: "es-BO",
+    chineseName: "西班牙语 - 玻利维亚"
+  },
+  { name: "Spanish - Chile", code: "es-CL", chineseName: "西班牙语 - 智利" },
+  {
+    name: "Spanish - Colombia",
+    code: "es-CO",
+    chineseName: "西班牙 - 哥伦比亚"
+  },
+  {
+    name: "Spanish - Costa Rica",
+    code: "es-CR",
+    chineseName: "西班牙语 - 哥斯达黎加"
+  },
+  {
+    name: "Spanish - Dominican Republic",
+    code: "es-DO",
+    chineseName: "西班牙语 - 多米尼加共和国"
+  },
+  {
+    name: "Spanish - Ecuador",
+    code: "es-EC",
+    chineseName: "西班牙语 - 厄瓜多尔"
+  },
+  {
+    name: "Spanish - El Salvador",
+    code: "es-SV",
+    chineseName: "西班牙语 - 萨尔瓦多"
+  },
+  {
+    name: "Spanish - Guatemala",
+    code: "es-GT",
+    chineseName: "西班牙语 - 危地马拉"
+  },
+  {
+    name: "Spanish - Honduras",
+    code: "es-HN",
+    chineseName: "西班牙语 - 洪都拉斯"
+  },
+  { name: "Spanish - Mexico", code: "es-MX", chineseName: "西班牙语 - 墨西哥" },
+  {
+    name: "Spanish - Nicaragua",
+    code: "es-NI",
+    chineseName: "西班牙语 - 尼加拉瓜"
+  },
+  { name: "Spanish - Panama", code: "es-PA", chineseName: "西班牙语 - 巴拿马" },
+  {
+    name: "Spanish - Paraguay",
+    code: "es-PY",
+    chineseName: "西班牙语 - 巴拉圭"
+  },
+  { name: "Spanish - Peru", code: "es-PE", chineseName: "西班牙语 - 秘鲁" },
+  {
+    name: "Spanish - Puerto Rico",
+    code: "es-PR",
+    chineseName: "西班牙语 - 波多黎各"
+  },
+  { name: "Spanish - Spain", code: "es-ES", chineseName: "西班牙语 - 西班牙" },
+  {
+    name: "Spanish - Uruguay",
+    code: "es-UY",
+    chineseName: "西班牙语 - 乌拉圭"
+  },
+  {
+    name: "Spanish - Venezuela",
+    code: "es-VE",
+    chineseName: "西班牙语 - 委内瑞拉"
+  },
+  {
+    name: "Swahili - Kenya",
+    code: "sw-KE",
+    chineseName: "斯瓦希里语 - 肯尼亚"
+  },
+  { name: "Swedish - Finland", code: "sv-FI", chineseName: "瑞典语 - 芬兰语" },
+  { name: "Swedish - Sweden", code: "sv-SE", chineseName: "瑞典语 - 瑞典语" },
+  { name: "Syriac - Syria", code: "syr-SY", chineseName: "叙利亚 - 叙利亚" },
+  { name: "Tamil - India", code: "ta-IN", chineseName: "泰米尔语 - 印度" },
+  { name: "Tatar - Russia", code: "tt-RU", chineseName: "鞑靼 - 俄罗斯" },
+  { name: "Telugu - India", code: "te-IN", chineseName: "泰卢固语 - 印度" },
+  { name: "Thai - Thailand", code: "th-TH", chineseName: "泰国 - 泰国" },
+  { name: "Turkish - Turkey", code: "tr-TR", chineseName: "土耳其 - 土耳其" },
+  {
+    name: "Ukrainian - Ukraine",
+    code: "uk-UA",
+    chineseName: "乌克兰 - 乌克兰"
+  },
+  {
+    name: "Urdu - Pakistan",
+    code: "ur-PK",
+    chineseName: "乌尔都语 - 巴基斯坦"
+  },
+  {
+    name: "Uzbek (Cyrillic) - Uzbekistan",
+    code: "Cy-uz-UZ",
+    chineseName: "乌兹别克语（西里尔文） - 乌兹别克斯坦"
+  },
+  {
+    name: "Uzbek (Latin) - Uzbekistan",
+    code: "Lt-uz-UZ",
+    chineseName: "乌兹别克语（拉丁语） - 乌兹别克斯坦"
+  },
+  { name: "Vietnamese - Vietnam", code: "vi-VN", chineseName: "越南 - 越南" }
+];
